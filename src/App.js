ca9da8e63a0a2e0d2d503362ff6dca358b4bdc9d
Konrad Dziurdź <kodz@gu160.local>
2019-03-13 15:07:27 +0100

    working push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axiosMain from "./lib/network/axiosMain";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    handleGetRooms = () => {
+        console.log('get rooms')
+        axiosMain.get('/api/1/rooms/')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    };
+
+    handleShowEnvVars = () => {
+        console.log('handleShowEnvVars')
+        console.log(process.env);
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <button onClick={this.handleGetRooms}>get rooms</button>
+                    <button onClick={this.handleShowEnvVars}>show env vars</button>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/lib/network/axiosMain.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const envMode = process.env.NODE_ENV;
+
+let baseURL;
+
+switch (envMode) {
+    case 'development': { // production
+        baseURL = 'https://samplespring.cfapps.io';
+        break;
+    }
+    default: {
+        baseURL = 'http://localhost:8080/'
+    }
+}
+
+const axiosMain = axios.create({
+    baseURL
+});
+
+export default axiosMain;

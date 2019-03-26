fb511fd324bc9aa1871c00ca63c20135f28c3b8c
Konrad Dziurdź <kodz@grapeup.com>
2019-03-26 16:46:50 +0100

    interceptor

--- src/lib/RoomConnection/Connection.js
+++ src/lib/RoomConnection/Connection.js
@@ -5,28 +5,29 @@ import {Subject} from 'rxjs'
 class Connection {
     onMessage = new Subject();
 
-    constructor(stompClient, room, userName) {
+    constructor(stompClient, room, userName, token) {
         this.stompClient = stompClient;
         this.room = room;
         this.userName = userName;
+        this.token = token;
         this.subscribeIncomingMessages();
         this.joinRoom();
     }
 
     subscribeIncomingMessages() {
         this.incomingMessagesSubscription = this.stompClient
-            .subscribe(`/topic/room.${this.room.id}`, this.onMessageReceived);
+            .subscribe(`/topic/room.${this.room.id}`, this.onMessageReceived, {'Authorization': this.token});
     }
 
     joinRoom() {
-        this.stompClient.send(`/ws-api/topic/room.${this.room.id}.addUser`, {},
+        this.stompClient.send(`/ws-api/topic/room.${this.room.id}.addUser`, {'Authorization': this.token},
             JSON.stringify({
                 plainText: this.userName
             }));
     }
 
     sendChatMessage(chatMessage) {
-        this.stompClient.send(`/ws-api/topic/room.${this.room.id}.message`, {},
+        this.stompClient.send(`/ws-api/topic/room.${this.room.id}.message`, {'Authorization': this.token},
             JSON.stringify({
                 plainText: chatMessage
             }));
--- src/lib/RoomConnection/ConnectionFactory.js
+++ src/lib/RoomConnection/ConnectionFactory.js
@@ -2,15 +2,19 @@ import SockJS from 'sockjs-client'
 import Stomp from 'stompjs'
 import Connection from "./Connection";
 
+const token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImV4cCI6MTU1NDQ2Mjc3OH0.29pAwypDT3MQLOLz70IRSSoPbddo8FwaIGCL7Nv1_4o-eQTPNDEC6zfzSTJeqFqOcIqKSuzj7cZUPxUzfv9Qiw';
+
 class ConnectionFactory {
 
 
     connect = (room, userName) => {
         return new Promise((resolve, reject) => {
-            const sock = new SockJS('/ws');
+            const sock = new SockJS('/ws?xxxx=yyyy');
             const stompClient = Stomp.over(sock);
-            stompClient.connect({}, (frame) => {
-                resolve(new Connection(stompClient, room, userName))
+            console.log('bylo over')
+            stompClient.connect({'Authorization': token}, (frame) => {
+                console.log('POLACZONY')
+                resolve(new Connection(stompClient, room, userName, token))
                 // console.log('Connected: ' + frame);
             });
         })
